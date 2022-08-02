0387fcf20a696eec4d6d556a6f2d14fe784e9d80
ShitalBarahate <110393511+ShitalBarahate@users.noreply.github.com>
2022-08-02 18:20:13 +0530

    Method m2 added
    check

--- GitHub project/src/basic/DemoGit.java
+++ GitHub project/src/basic/DemoGit.java
@@ -6,6 +6,10 @@ public class DemoGit {
 	{
 		System.out.println("m1...method");
 	}
+	public void m2()
+	{
+	System.out.println("m2 method added...")
+	}
 	
 	public static void main(String[] args)
 	{
