d95d39393e96dff7e70cee2006af49b1b9b935f4
PunamNikam <110393278+PunamNikam@users.noreply.github.com>
2022-08-02 11:32:32 +0530

    By git team

--- GitHub project/src/basic/DemoGit.java
+++ GitHub project/src/basic/DemoGit.java
@@ -2,10 +2,14 @@ package basic;
 
 public class DemoGit {
 
+	public void  m1()
+	{
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("hiii its gitHub*****");
 		System.out.println(" change in git hub....");
 		System.out.println("new change");
+		
 	}
 }
